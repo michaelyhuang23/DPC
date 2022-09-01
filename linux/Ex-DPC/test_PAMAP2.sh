2402e777d6ebfeca63036c92c40b296e90b5df79
Michael Huang <michaelyhuang23@gmail.com>
2022-09-01 05:41:23 +0000

    Ex-DPC PAMAP2

---
+++ linux/Ex-DPC/test_PAMAP2.sh
@@ -0,0 +1,19 @@
+r=0.02
+
+for((t=1; t<=3; t+=1)); do
+    echo $t
+    for((s=1; s<=16; s*=2)); do
+        echo ${s}
+        cp ../../../ParCluster/dataset/reallife/PAMAP2.dat ../_dataset/synthetic.txt
+        echo ${r} > ../_parameter/cutoff.txt
+        echo ${s} > ../_parameter/thread_num.txt
+        ./exdpc.out > results/PAMAP2${t}/orig_${r}_${s}.txt
+    done
+    for((s=30; s<=60; s*=2)); do
+        echo ${s}
+        cp ../../../ParCluster/dataset/reallife/PAMAP2.dat ../_dataset/synthetic.txt
+        echo ${r} > ../_parameter/cutoff.txt
+        echo ${s} > ../_parameter/thread_num.txt
+        ./exdpc.out > results/PAMAP2${t}/orig_${r}_${s}.txt
+    done
+done

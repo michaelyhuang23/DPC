540674e3f59c6daa5c719ae1d62e1f6752319620
Michael Huang <michaelyhuang23@gmail.com>
2022-12-19 19:12:07 -0500

    local stuffs

--- linux/Ex-DPC/test_query.sh
+++ linux/Ex-DPC/test_query.sh
@@ -7,13 +7,13 @@ for((t=1; t<=3; t+=1)); do
         cp ../../../ParCluster/dataset/reallife/query.pbbs ../_dataset/synthetic.txt
         echo ${r} > ../_parameter/cutoff.txt
         echo ${s} > ../_parameter/thread_num.txt
-        ./exdpc.out > results/query${t}/orig_${r}_${s}.txt
+        ./exdpc.out > results/query_local${t}/orig_${r}_${s}.txt
     done
     for((s=30; s<=60; s*=2)); do
         echo ${s}
         cp ../../../ParCluster/dataset/reallife/query.pbbs ../_dataset/synthetic.txt
         echo ${r} > ../_parameter/cutoff.txt
         echo ${s} > ../_parameter/thread_num.txt
-        ./exdpc.out > results/query${t}/orig_${r}_${s}.txt
+        ./exdpc.out > results/query_local${t}/orig_${r}_${s}.txt
     done
 done
